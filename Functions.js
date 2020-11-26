5043bbcf4ca78125a7a9a04486b4787c7f80e7dc
shandipta-chamling <i213096@d2hs.com>
2020-11-26 10:00:05 +0545

    About JS functions nov 26 2020

---
+++ Functions.js
@@ -0,0 +1,17 @@
+function square(number){
+
+   let sq =  number*number;
+   return sq;
+}
+
+console.log(square(100));
+let s = square(9);
+console.log(s);
+
+//anonymous function
+var f1 =function (number){
+    let s =  number*number;
+    return s;
+ }
+
+ console.log(f1(10));
