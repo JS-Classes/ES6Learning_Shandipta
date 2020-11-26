28f4497ead164138cc75b98ae25bfc9107557138
shandipta-chamling <i213096@d2hs.com>
2020-11-26 09:44:52 +0545

    VarConstLet difference Nov 26 2020

--- VariableDeclarations.js
+++ VariableDeclarations.js
@@ -2,15 +2,24 @@
 
 //Primary
 var x = 150;
-var y;
-y="Test";
-y= true;
-y=123;
-var a= 2n ** 200n;
-console.log(a);
-var z = undefined;
-var b = null;
+// var y;
+// y="Test";
+// y= true;
+// y=123;
+// var a= 2n ** 200n;
+// console.log(a);
+// var z = undefined;
+// var b = null;
 
-//Structural
-var c = [1,2,3,4,5];
-var d = {name:"Anna", isFemale=true};
+// //Structural
+// var c = [1,2,3,4,5];
+// var d = {name:"Anna", isFemale=true};
+
+//numbers builtin constants
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// var c1 = +Infinity;
+// var c2 = -Infinity;
+// var c3 = NaN; //not a number. if string is divided by a number then it is NAN 
+// var c4 = "abcde"/3;
+console.log(typeof x);
