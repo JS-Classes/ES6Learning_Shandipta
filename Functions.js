8e7994e5ed09a0b38418f6d3de459cbf2588868e
shandipta-chamling <i213096@d2hs.com>
2020-11-27 09:57:45 +0545

    Functions type Nov 27 2020

--- Functions.js
+++ Functions.js
@@ -15,3 +15,26 @@ var f1 =function (number){
  }
 
  console.log(f1(10));
+
+ //Closure
+ function outside(x){
+    function inside(y){
+       return x+y;
+    }
+    return inside;
+ }
+
+ let inside_fn = outside(10);
+ let result = inside_fn(20);
+ console.log(result);
+
+ function addSquare(a,b){
+    function square(x){
+       return x*x;
+    }
+    let sq = square(a)+square(b);
+    return sq;
+ }
+
+ let sqResultOut= addSquare(2,2);
+ console.log(sqResultOut);
